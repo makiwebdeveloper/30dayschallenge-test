af73fe935f82e1353f91ce3e7a718eac3d83d527
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-11-23 18:01:51 +0100

    swagger + env

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('30dayschallenge server')
+    .setDescription('Api documentation')
+    .setVersion('1.0.0')
+    .addTag('makiwebdeveloper')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
