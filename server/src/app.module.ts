9b2800a29ef468142b45b785a40f1ad3e6e74f96
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-11-23 18:46:19 +0100

    setup prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    DbModule,
   ],
   controllers: [AppController],
   providers: [AppService],
